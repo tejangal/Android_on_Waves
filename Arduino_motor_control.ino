469fa6492caf1fe9421d9f412d1ee437dce15350
tejangal <tejangal@pdx.edu>
2015-08-04 11:53:38 -0700

    External Arduino test1 program
    The program is tested over Arduino uno when it is not present inside the
    boat, temporary connections are made to test the program. Program tested
    successfully

---
+++ Arduino_motor_control.ino
@@ -0,0 +1,102 @@
+
+//Copyright (c) 2015 Tejaswini Angal
+/* This program is released under the "GNU General Public License(GPL)".
+   Please see the file COPYING in this distribution for
+   license terms. */
+
+//This program controls the motion of the boat in three directions
+//1.Forward
+//2.Left
+//3.Right
+#include <MeetAndroid.h>
+//Pin setup
+int led = 13;                    //lights the led on arduino, used for testing purpose
+int command = 0;                //used to capture the recieved data,when no data is received, value remains '0'
+int pwmLeftPin = 3;            //this pin 3 on arduino uno switches the left motor on and off
+int pwmRightPin = 11;         //this pin 11 on arduino uno switches thge right motor on and off 
+int directionLeftPin = 12;   //controls the direction of left motor(forward/reverse)
+int directionRightPin = 13;  //controls the direction of the right motor(forward/reverse)
+
+void setup()
+{
+  Serial.begin(115200);            //set the baud rate
+  Serial.flush();                  //flush the pipe 
+    
+  pinMode(led, OUTPUT);             //declare respective output pins
+  pinMode(pwmLeftPin,OUTPUT);
+  pinMode(pwmRightPin,OUTPUT);
+  pinMode(directionLeftPin,OUTPUT);
+  pinMode(directionRightPin,OUTPUT);
+ 
+  analogWrite(pwmRightPin,0);          //set initial values to respective pins
+  analogWrite(pwmLeftPin,0);
+  digitalWrite(directionRightPin,0);        
+  digitalWrite(directionLeftPin,0);
+  
+}
+
+void loop()
+{
+  Serial.flush();
+// If bluetooth received characters copy it in command
+  while (Serial.available()>0)  //while characters are arriving over bluetooth     
+  {
+    
+  int rcvData = 0;  
+  rcvData = Serial.read();
+  command = rcvData; 
+  }
+   
+       if(command == 65 )
+      {
+       motorA();  //switch on motor1
+      }
+      else if(command == 66)
+      {
+         motorB();   //switch on motor2
+      }  
+      else if(command == 67)
+      {  //switch off both motors    
+        analogWrite(pwmRightPin,0);
+        analogWrite(pwmLeftPin,0);
+        digitalWrite(directionRightPin,0);        
+        digitalWrite(directionLeftPin,0);
+      }
+      else if(command == 68)
+      {
+        motorAll();                  //switch on both the motors
+        
+      }
+      else
+      {
+        analogWrite(pwmRightPin,0);            //set respective initial value
+        analogWrite(pwmLeftPin,0);
+        digitalWrite(directionRightPin,0);        
+        digitalWrite(directionLeftPin,0);       
+      }
+   
+}
+
+void motorA()
+{ 
+  analogWrite(pwmLeftPin,160);      //voltage 1.6V
+  analogWrite(pwmRightPin,0);
+  digitalWrite(directionLeftPin,0);  //direction= forward
+  digitalWrite(directionRightPin,0);
+}
+
+void motorB()
+{
+ analogWrite(pwmRightPin,160);     //voltage 1.6V  
+ analogWrite(pwmLeftPin,0);
+ digitalWrite(directionRightPin,0);  //direction = forward      
+ digitalWrite(directionLeftPin,0);
+}
+
+void motorAll()
+{
+ analogWrite(pwmLeftPin,160);      //voltage 1.6V
+ digitalWrite(directionLeftPin,0);  //direction= forward
+ analogWrite(pwmRightPin,160);     //voltage 1.6V  
+ digitalWrite(directionRightPin,0);  //direction = forward      
+}
